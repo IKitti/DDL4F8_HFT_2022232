02cab0a7450b232baffa03b60465bf951a54178f
IKitti <ivanyikitti2020@gmail.com>
2023-12-02 14:46:30 +0100

    Repositorys creating ready

--- DDL4F8_HFT_2022232.Repository/ClassRepo/PetownerRepository.cs
+++ DDL4F8_HFT_2022232.Repository/ClassRepo/PetownerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DDL4F8_HFT_2022232.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,29 @@ using System.Threading.Tasks;
 
 namespace DDL4F8_HFT_2022232.Repository.ClassRepo
 {
-    internal class PetownerRepository
+    public class PetownerRepository : Repository<Petowner>, IRepository<Petowner>
     {
+        public PetownerRepository(PetsDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Petowner Read(int id)
+        {
+            return ctx.PetOwners.FirstOrDefault(t => t.Id == id);
+        }
+
+        public override void Update(Petowner item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
+            }
+
+            ctx.SaveChanges();
+        }
     }
 }
