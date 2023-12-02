1e30222bed39b908745b10e67f23524a8d7410ea
IKitti <ivanyikitti2020@gmail.com>
2023-12-02 15:17:34 +0100

    Test class created

--- DDL4F8_HFT_2022232.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DDL4F8_HFT_2022232.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ DDL4F8_HFT_2022232.Test/PetLogicTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDL4F8_HFT_2022232.Test
+{
+    internal class PetLogicTest
+    {
+    }
+}
