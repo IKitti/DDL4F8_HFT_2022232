f09bc917c086c80927fc4ec764dbc5ea7af03895
IKitti <ivanyikitti2020@gmail.com>
2023-12-05 14:20:51 +0100

    PetController create

--- DDL4F8_HFT_2022232.Endpoint/Contollers/PetController.cs
+++ DDL4F8_HFT_2022232.Endpoint/Contollers/PetController.cs
@@ -1,6 +1,47 @@
-﻿namespace DDL4F8_HFT_2022232.Endpoint.Contollers
+﻿using DDL4F8_HFT_2022232.Logic.ClassLogicInterfaces;
+using DDL4F8_HFT_2022232.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DDL4F8_HFT_2022232.Endpoint.Contollers
 {
-    public class PetController
+    [Route("api/pet")]
+    public class PetController : Controller
     {
+        private readonly IPetLogic logic;
+        public PetController(IPetLogic _logic)
+        {
+            this.logic = _logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Pet> ReadAll()
+        {
+            return logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Pet Read(int id)
+        {
+            return logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Pet item)
+        {
+            logic.Create(item);
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, [FromBody] Pet item)
+        {
+            logic.Update(item);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            logic.Delete(id);
+        }
     }
 }
