f441eaf66182bdb62e6041ec490d150bd471dbbe
IKitti <ivanyikitti2020@gmail.com>
2023-12-05 14:17:50 +0100

    PetFoodController create

--- DDL4F8_HFT_2022232.Endpoint/Contollers/PetFoodController.cs
+++ DDL4F8_HFT_2022232.Endpoint/Contollers/PetFoodController.cs
@@ -1,6 +1,48 @@
-﻿namespace DDL4F8_HFT_2022232.Endpoint.Contollers
+﻿using DDL4F8_HFT_2022232.Logic.ClassLogicInterfaces;
+using DDL4F8_HFT_2022232.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DDL4F8_HFT_2022232.Endpoint.Contollers
 {
-    public class PetFoodController
+    [Route("api/food")]
+    public class PetFoodController : Controller
     {
+        private readonly IPetFoodLogic logic;
+
+        public PetFoodController(IPetFoodLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<PetFood> ReadAll()
+        {
+            return logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public PetFood Read(int id)
+        {
+            return logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] PetFood item)
+        {
+            logic.Create(item);
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, [FromBody] PetFood item)
+        {
+            logic.Update(item);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            logic.Delete(id);
+        }
     }
 }
